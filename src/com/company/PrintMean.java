f70f1706a7e6a0d98c127d26192f598a4df7e6eb
Julius Karsbrink <Julius.karsbrink@student.kyh.se>
2020-09-10 15:18:19 +0200

    Clean Master

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -3,8 +3,6 @@ package com.company;
 public class PrintMean {
 
     public static void main(String[] args) {
-        // Heltalsdivision (skriver ut int istället för double)
-        //Test
         System.out.println("Mean of 3, 1, 3, 3: " + meanCalc(3, 1, 3, 3));
         System.out.println("Mean of 5, 1, 3, 3: " + meanCalc(5, 1, 3, 3));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1, -2, 3, -4));
@@ -14,7 +12,6 @@ public class PrintMean {
     }
 
     public static String meanCalc(int a, int b, int c, int d) {
-        // Gör en sträng av "mean"
 
         String mean = String.valueOf((a + b + c + d) / 4.0);
         String meanString = mean;
